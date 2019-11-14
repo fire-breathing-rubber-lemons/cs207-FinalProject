919d43d374bd9745247dc1e4006bb71fe7a3ecd9
Hayden <hayden@dhcp-10-250-228-255.harvard.edu>
2019-11-14 18:30:05 -0500

    Adding initial docstrings to pyad classes and function
    Utilising the numpy style for docstring as we are using numpy as the base elementary function class - https://numpydoc.readthedocs.io/en/latest/format.html

--- pyad/forward_diff.py
+++ pyad/forward_diff.py
@@ -1,18 +1,56 @@
 import numpy as np
 
-
 class MultivariateDerivative:
+    '''
+    Multivariate Derivative is a class called automatically by the Variable
+    or Tensor classes to hold derivative information. 
+
+    Parameters
+    ----------
+    variables : dict
+        Store the name and value of the derivative object allowing for multiple variable keys
+    
+    Attributes
+    ----------
+    variables : dict
+        Internal storage of the named derivative values in dictionary format
+    '''
     def __init__(self, variables=None):
         self.variables = variables or {}
 
     def __repr__(self):
+        '''
+        Create a descriptive object for printing, D noting that the object is a derivative
+
+        Returns
+        -------
+        str
+            a string representing the value of the derivative of the MultivariateDerivative object
+        '''
         values = ', '.join(f'{k}={v:.3g}' for k, v in self.variables.items())
         return f'D({values})'
 
     def copy(self):
+        '''
+        Given an existing MultivariateDerivative object (self) create a new one as a copy
+
+        Returns
+        -------
+        MultivariateDerivative
+            a new MultivariateDerivative object
+        '''
         return MultivariateDerivative(self.variables.copy())
 
     def __getitem__(self, key):
+        '''
+        Access the derivative of one of the variables in the object, print(MultivariateDerivative)
+        will reveal the existing keys within the instance.
+
+        Returns
+        -------
+        dict value
+            Value of the dictionary of derivatives at the specified key
+        '''
         return self.variables[key]
 
     # the only way to combine derivatives is with addition or multiplication
@@ -31,7 +69,30 @@ class MultivariateDerivative:
 
 
 class Tensor:
+    '''
+    Tensor (constant value) is the base class of the Variable and can be used to represent 
+    constant values where the derivative is not required for automatic computation during
+    automatic differentiation. Derivative by default is set to an empty dictionary.
+
+    Parameters
+    ----------
+    value : np.array
+        Store the value of the constant
+
+    d : MultivariateDerivative
+        Store the value of the derivative (default is an empty MultivariateDerivative)
+        but can be manually set to a specific value
+    
+    Attributes
+    ----------
+    value : np.array
+        Internal storage of the tensor value
+
+    d : MultivariateDerivative
+        Internal storage of the tensor derivative
+    '''
     def __init__(self, value, d=None):
+        # If existing Tensor check for whether a derivative value was set
         if isinstance(value, Tensor):
             value, d = value.value, value.d.copy()
 
@@ -98,6 +159,36 @@ class Tensor:
 
 
 class Variable(Tensor):
+    '''
+    Variable (a variable value for the purposes of differentiation). A variable is used to
+    represent any value which the user wishes to include in the partial derivative outputs
+    and is built on top of the Tensor base class.
+
+    As a variable a name parameter is required (example: x, y or z).
+
+    Parameters
+    ----------
+    name: str
+        Store the name of the variable
+
+    value : np.array
+        Store the value of the variable
+
+    d : MultivariateDerivative
+        Store the value of the derivative, default is an array of 1's (unit vectors) which
+        will result in the computation of the Jacobian
+    
+    Attributes
+    ----------
+    name: str
+        Internal storage of the variable name
+
+    value : np.array
+        Internal storage of the variable value
+
+    d : MultivariateDerivative
+        Internal storage of the variable derivative
+    '''
     def __init__(self, name, value):
         self.name = name
         self.value = np.array(value)
@@ -108,20 +199,84 @@ class Variable(Tensor):
 
 # Elementary operations of a single variable. They all use chain rule.
 def _elementary_op(obj, fn, deriv_fn):
+    '''
+    A generic framework to allow for the chain rule of other elementary functions 
+    taken from the numpy module.
+
+    Parameters
+    ----------
+    obj : Class
+        The Variable or Tensor which the elementary function is being differentiated at
+
+    fn : np.function
+        The elementary function from the numpy module
+
+    deriv_fun:  np.function
+        The prespecified derivative of the given numpy elementary function
+
+
+    Returns
+    -------
+    Tensor: class
+        Tensor object which contains the resulting value and result from the
+        chain rule (new derivative)
+    '''
     v, d = Tensor.get_value_and_deriv(obj)
     chain_rule = d.mul(deriv_fn(v))
     return Tensor(fn(v), chain_rule)
 
 
 def sin(tensor):
+    '''
+    pyad sin - to calculate a Tensor (value and derivative) of the sin function
+        sin differentiates to cosine
+
+    Parameters
+    ----------
+    tensor : class
+        The value of the variable or constant which the sin function is being differentiated at
+
+    Returns
+    -------
+    Tensor: class
+        Calls the _elementary_op function and returns the resulting Tensor
+    '''
     return _elementary_op(tensor, np.sin, np.cos)
 
 
 def cos(tensor):
+    '''
+    pyad cos - to calculate a Tensor (value and derivative) of the cos function
+        cosine differentiates to minus sin
+
+    Parameters
+    ----------
+    tensor : class
+        The value of the variable or constant which the cosine function is being differentiated at
+
+    Returns
+    -------
+    Tensor: class
+        Calls the _elementary_op function and returns the resulting Tensor
+    '''
     return _elementary_op(tensor, np.cos, lambda x: -np.sin(x))
 
 
 def tan(tensor):
+    '''
+    pyad cos - to calculate a Tensor (value and derivative) of the tan function
+        tan differentiates to sec^2(x) (1/cos^2(x))
+
+    Parameters
+    ----------
+    tensor : class
+        The value of the variable or constant which the tan function is being differentiated at
+
+    Returns
+    -------
+    Tensor: class
+        Calls the _elementary_op function and returns the resulting Tensor
+    '''
     return _elementary_op(tensor, np.tan, lambda x: 1 / (np.cos(x) ** 2))
 
 
