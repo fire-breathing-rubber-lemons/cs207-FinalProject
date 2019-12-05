331bf0b12e502bcc0483b6c3d22b867dcaa7851b
Catharine Wu <cathwu225@berkeley.edu>
2019-12-04 20:56:44 -0500

    fix import issue-put illustrative test in reverse_mode.py for now

--- pyad/reverse_mode.py
+++ pyad/reverse_mode.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 
 class Reverse:
@@ -146,3 +145,23 @@ def sqrt(x):
 
 def cbrt(x):
 	return _elementary_op(x, np.cbrt, lambda x: 1 / (3 * x**(2/3)))
+
+
+# Simple test suite explaining how Reverse works
+#Initialization
+x = Reverse(0.5)
+y = Reverse(4.2)
+z = Reverse(3)
+f = x * y**3 + sin(x) - logistic(z)
+
+#set df seed to be 1
+f.grad_value = 1.0
+
+f_val = f.value
+x_val = x.value
+x_grad = x.get_grad()
+y_grad = y.get_grad()
+z_grad = z.get_grad()
+
+assert abs(f.value - (0.5*4.2**3+np.sin(0.5) - 1/(1+np.exp(-3)))) <= 1e-15
+assert abs(x.get_grad() - (4.2**3 + np.cos(0.5))) <= 1e-15
