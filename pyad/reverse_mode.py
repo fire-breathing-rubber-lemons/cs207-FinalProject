854cf7e478a87267ffacda1b41f3a1855caf673d
Nenya Edjah <nenyaedjah@gmail.com>
2019-12-08 06:04:03 -0500

    added matrix multiplication support to reverse mode

--- pyad/reverse_mode.py
+++ pyad/reverse_mode.py
@@ -7,15 +7,25 @@ class Tensor:
     """
 
     def __init__(self, value):
-        self.value = value
-        self.children = []
-        self.grad_value = None
+        if isinstance(value, Tensor):
+            self.value = np.array(value.value)
+            self.children = value.children.copy()
+            self.grad_value = None
+        else:
+            self.value = np.array(value)
+            self.children = []
+            self.grad_value = None
+
+        if len(self.value.shape) > 2:
+            raise ValueError('Can only support 0-D, 1-D, and 2-D Tensors')
 
     def backward(self):
         """
         A function that seeds in the derivative of a function with respect to itself, i.e. df/df = 1
         """
-        self.grad_value = 1.0
+        if self.value.shape != ():
+            raise Exception('Cannot call .backward() on a non-scalar')
+        self.grad_value = np.array(1.0)
 
     @property
     def grad(self):
@@ -23,8 +33,12 @@ class Tensor:
         A function that computes the gradient value using the chain rule
         """
         if self.grad_value is None:
-            # recurse only if the value is not yet cached
-            self.grad_value = sum(weight * var.grad for weight, var in self.children)
+            self.grad_value = 0
+            for weight, node in self.children:
+                if callable(weight):
+                    self.grad_value += weight(node.grad)
+                else:
+                    self.grad_value += weight * node.grad
         return self.grad_value
 
     # Comparisons work like they do in numpy. Derivative information is ignored
@@ -55,9 +69,36 @@ class Tensor:
 
     def __bool__(self):
         return bool(self.value)
-    
+
     def __repr__(self):
         return f'Tensor({self.value}, D({self.grad_value}))'
+
+    @property
+    def shape(self):
+        return self.value.shape
+
+    def sum(self):
+        z = Tensor(np.sum(self.value))
+        self.children.append((np.ones(self.shape), z))
+        return z
+
+    def prod(self):
+        if self.shape == ():
+            return self
+
+        forward_prod = np.cumprod(self.value.flatten())
+        backward_prod = np.cumprod(self.value.flatten()[::-1])[::-1]
+        result = np.ones(len(forward_prod))
+
+        for i in range(len(forward_prod)):
+            if i != 0:
+                result[i] *= forward_prod[i - 1]
+            if i != len(forward_prod) - 1:
+                result[i] *= backward_prod[i + 1]
+
+        z = Tensor(np.prod(self.value))
+        self.children.append((result.reshape(self.shape), z))
+        return z
 
     def __neg__(self):
         return self.__mul__(-1)
@@ -73,12 +114,10 @@ class Tensor:
         return self.__add__(other)
 
     def __sub__(self, other):
-        sub = other.__neg__()
-        return self.__add__(sub)
+        return self + (-other)
 
     def __rsub__(self, other):
-        sub = self.__neg__()
-        return sub + other
+        return (-self) + other
 
     def __mul__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
@@ -88,34 +127,59 @@ class Tensor:
         return z
 
     def __rmul__(self, other):
-        return self.__mul__(other)
+        return self * other
 
     def __truediv__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
         z = Tensor(self.value / other.value)
-        self.children.append((1/other.value, z))
-        other.children.append((-self.value/other.value**2, z))
+        self.children.append((1 / other.value, z))
+        other.children.append((-self.value / other.value**2, z))
         return z
 
     def __rtruediv__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
-        z = Tensor(other.value / self.value)
-        self.children.append((-other.value/self.value**2, z))
-        other.children.append((1/self.value, z))
-        return z
+        return other / self
 
     def __pow__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
-        z = Tensor(self.value**other.value)
-        self.children.append((other.value*self.value**(other.value-1), z))
-        other.children.append((self.value**other.value*np.log(self.value), z))
+        z = Tensor(self.value ** other.value)
+        self.children.append((other.value * self.value**(other.value - 1), z))
+        other.children.append((self.value**other.value * np.log(self.value), z))
         return z
 
     def __rpow__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
-        z = Tensor(other.value**self.value)
-        self.children.append((other.value**self.value*np.log(other.value), z))
+        return other ** self
+
+    def _to_2d(self, x):
+        """
+        A helper function for reshaping numpy arrays as 2D matrices
+        """
+        if x.shape == ():
+            return x.reshape(1, 1)
+        return x.reshape(len(x), -1)
+
+    def __matmul__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+
+        def mm_backward_self(out_grad):
+            other_mat = self._to_2d(other.value)
+            out_grad_mat = self._to_2d(out_grad)
+            return (out_grad_mat @ other_mat.T).reshape(self.value.shape)
+
+        def mm_backward_other(out_grad):
+            self_mat = self._to_2d(self.value)
+            out_grad_mat = self._to_2d(out_grad)
+            return (self_mat.T @ out_grad_mat).reshape(other.value.shape)
+
+        z = Tensor(self.value @ other.value)
+        self.children.append((mm_backward_self, z))
+        other.children.append((mm_backward_other, z))
         return z
+
+    def __rmatmul__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        return other @ self
 
 
 # Elementary functions
@@ -176,7 +240,7 @@ def cosh(x):
 
 
 def tanh(x):
-    return _elementary_op(x, np.tanh, lambda x: 1/(np.cosh(x)**2))
+    return _elementary_op(x, np.tanh, lambda x: 1 / (np.cosh(x) ** 2))
 
 
 def abs(x):
@@ -192,7 +256,9 @@ def logistic(x):
 
 
 def log(x, base=np.e):
-    return _elementary_op(x, np.log, lambda x: 1 / x) / np.log(base)
+    if base == np.e:
+        return _elementary_op(x, np.log, lambda x: 1 / x)
+    return log(x) / log(base)
 
 
 def log2(x):
@@ -208,4 +274,4 @@ def sqrt(x):
 
 
 def cbrt(x):
-    return _elementary_op(x, np.cbrt, lambda x: 1 / (3 * x**(2/3)))
+    return _elementary_op(x, np.cbrt, lambda x: 1 / (3 * x ** (2/3)))
