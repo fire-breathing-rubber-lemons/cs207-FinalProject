690e220224300a406c0c68e9eb0473feb2bb29ad
Hayden <hayden@Haydens-MacBook-Pro.local>
2019-12-08 21:11:12 -0500

    fixing indent in tests caused by merge conflict

--- pyad/tests/test_reverse.py
+++ pyad/tests/test_reverse.py
@@ -8,18 +8,18 @@ import pytest
 #       End to End Tests       #
 ################################
 def test_reverse_by_example():
-	  x = rev.Tensor(0.5)
-	  y = rev.Tensor(4.2)
-	  z = rev.Tensor(3)
-	  f = x * y**3 + rev.sin(x) - rev.logistic(z)
+	x = rev.Tensor(0.5)
+	y = rev.Tensor(4.2)
+	z = rev.Tensor(3)
+	f = x * y**3 + rev.sin(x) - rev.logistic(z)
 
     # set df seed
-    f.backward()
+	f.backward()
 
-	  assert abs(f.value - (0.5*4.2**3+np.sin(0.5) - 1/(1+np.exp(-3)))) <= 1e-15
-	  assert abs(x.grad - (4.2**3 + np.cos(0.5))) <= 1e-15
-	  assert abs(y.grad - (3*0.5*4.2**2)) <= 1e-15
-	  assert abs(z.grad - (-np.exp(-3)/(1+np.exp(-3))**2)) <= 1e-15
+	assert abs(f.value - (0.5*4.2**3+np.sin(0.5) - 1/(1+np.exp(-3)))) <= 1e-15
+	assert abs(x.grad - (4.2**3 + np.cos(0.5))) <= 1e-15
+	assert abs(y.grad - (3*0.5*4.2**2)) <= 1e-15
+	assert abs(z.grad - (-np.exp(-3)/(1+np.exp(-3))**2)) <= 1e-15
 
 
 def test_case_base_trigonometric():
